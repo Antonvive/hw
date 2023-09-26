9cb7d597ac87605215f0cd5ae99288879f9c3493
Anton <assddsd>
2023-09-26 14:12:24 +0500

    task 10

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью 
+//цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число");
+int number= Convert.ToInt32(Console.ReadLine());
+int Third (int num)
+{
+    int result=-1;
+    if (num >=100)
+    {
+     while (num > 999)
+     {
+        num = num / 10;
+     }
+        result = num % 10;
+    }
+ 
+return result; 
+}
+ 
+if (Third(number) == -1)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine($" Третья цифра -> {Third(number)}");
--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 15: Напишите программу, которая принимает 
+//на вход цифру, обозначающую день недели, и проверяет, 
+//является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine("Введите число");
+int number= Convert.ToInt32(Console.ReadLine());
+int Third (int num)
+{
+    if (num>5)
+    {
+        Console.WriteLine("выходной. (Да)");
+    }
+    else 
+    {
+        Console.WriteLine("раочий день. (Нет)");
+    }
+return num;
+}
+Third(number);
